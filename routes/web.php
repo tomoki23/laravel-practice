f886acf19cd8efa5b61e79601ee92c9a5a3ef8be
tomoki23 <tmdesign.0223@gmail.com>
2023-02-11 15:32:01 +0900

    ルート修正

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tasks', function () {
-    return view('index');
-});
+Route::get('/tasks', [TaskController::class, 'index'])->name('index');

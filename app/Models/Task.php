d1c90c0df873ba26504ef13a9361b4935b62849a
tomoki23 <tmdesign.0223@gmail.com>
2023-02-13 13:15:02 +0900

    登録処理追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'todo',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function taskRegister($task)
+    {
+        $this->create([
+            'todo' => $task['todo']
+        ]);
+
+    }
 }

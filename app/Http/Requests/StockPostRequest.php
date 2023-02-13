57949aa468dba8031de1c7edb702c8f3962591dd
tomoki23 <tmdesign.0223@gmail.com>
2023-02-13 20:26:23 +0900

    バリデーションルール作成

--- app/Http/Requests/StockPostRequest.php
+++ app/Http/Requests/StockPostRequest.php
@@ -13,7 +13,7 @@ class StockPostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class StockPostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'todo' => 'required|max:255'
         ];
     }
 }
